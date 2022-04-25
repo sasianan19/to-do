79e4fc9efcd8e4e422827224dd199a2f8f586e86
sasianan19 <sasianan19@gmail.com>
2022-04-25 02:05:38 -0600

    create Note model

--- todo/models.py
+++ todo/models.py
@@ -6,3 +6,6 @@ class Task(models.Model):
     # An ID field is automatically added to all Django models
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
+
+class Note(models.Model):
+    text = models.CharField(max_length=255)
